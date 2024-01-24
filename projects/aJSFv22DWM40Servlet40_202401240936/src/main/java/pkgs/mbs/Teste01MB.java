a6f741bcc086c36f6d0f02a3cf97d3b89c4b494a
marciomaiabr <marcio.luis.maia@gmail.com>
2024-01-24 10:13:06 -0400

    adding aJSFv22DWM40Servlet40_202401240936 project

---
+++ projects/aJSFv22DWM40Servlet40_202401240936/src/main/java/pkgs/mbs/Teste01MB.java
@@ -0,0 +1,28 @@
+package pkgs.mbs;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Teste01MB {
+
+	public Teste01MB() {
+		System.out.println("Teste01MB.Teste01MB()");
+	}
+
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("Teste01MB.postConstruct()");
+	}
+
+	public long getVersion() {
+		return 202401240745l;
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		System.out.println("Teste01MB.preDestroy()");
+	}
+
+}
